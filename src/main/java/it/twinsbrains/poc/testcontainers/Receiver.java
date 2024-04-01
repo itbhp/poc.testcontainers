20d06702605f5f29cc1ccb42e1ed124a86509f45
Paolo Sciarra <itbhps@gmail.com>
2024-04-01 08:50:41 +0200

    reformat

--- src/main/java/it/twinsbrains/poc/testcontainers/Receiver.java
+++ src/main/java/it/twinsbrains/poc/testcontainers/Receiver.java
@@ -3,21 +3,26 @@ package it.twinsbrains.poc.testcontainers;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 @SuppressWarnings("unused")
 public class Receiver {
 
-    private final CountDownLatch latch = new CountDownLatch(1);
+    private AtomicReference<CountDownLatch> latch = new AtomicReference<>(new CountDownLatch(1));
 
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        latch.countDown();
+        latch.get().countDown();
     }
 
 
     public CountDownLatch getLatch() {
-        return latch;
+        return latch.get();
+    }
+
+    public void reset() {
+        latch.set(new CountDownLatch(1));
     }
 
 }
--- src/test/java/it/twinsbrains/poc/testcontainers/MessagingIT.java
+++ src/test/java/it/twinsbrains/poc/testcontainers/MessagingIT.java
@@ -5,6 +5,8 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,17 +26,19 @@ import org.testcontainers.utility.DockerImageName;
 public class MessagingIT {
   @Container
   public static RabbitMQContainer rabbitMQContainer =
-      new RabbitMQContainer(
-              DockerImageName.parse("rabbitmq").withTag("3.7.25-management-alpine")
-      );
+      new RabbitMQContainer(DockerImageName.parse("rabbitmq").withTag("3.7.25-management-alpine"));
 
   @DynamicPropertySource
   static void properties(DynamicPropertyRegistry registry) {
     registry.add("spring.rabbitmq.port", () -> rabbitMQContainer.getMappedPort(5672));
   }
 
-  @Autowired
-  private Receiver receiver;
+  @Autowired private Receiver receiver;
+
+  @AfterEach
+  void tearDown() {
+    receiver.reset();
+  }
 
   @Test
   void it_should_work(CapturedOutput output) throws InterruptedException {
